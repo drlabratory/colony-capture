c46a4d48d7004711f0b1b49532ba93a847447993
Jeramia Ory <Jeramia Ory>
2016-05-21 21:31:06 -0700

    adding timestamp

--- LED-picture.py
+++ LED-picture.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
 from picamera import PiCamera
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -13,6 +14,7 @@ def picture_capture(filename):
     GPIO.output(18,GPIO.HIGH) # turn on LED lights
     camera.start_preview()
     sleep(5) # camera needs a few seconds to adjust levels
+    camera.annotate_text = time.strftime("%c")
     camera.capture(filename) # capture image to filename
     camera.stop_preview()
     GPIO.output(18,GPIO.LOW) # turn off LED lights
